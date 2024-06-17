e7cde816e7754ee975addb9f0ae218745fe88996
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-06-17 13:41:02 -0300

    Unsubscribe.

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
-import { Acao } from './modelo/acoes';
+import { Acao, AcoesAPI } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) { }
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes').pipe(
       tap((valor) => console.log(valor)),
       pluck('payload'), // posso usar o map(valor.payload)
       map((acoes) =>
